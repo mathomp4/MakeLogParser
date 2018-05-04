512d2f34e04888af946b77f154d59fcaab9ce2cd
Matthew Thompson <matthew.thompson@nasa.gov>
2018-05-04 07:10:57 -0400

    Add cmake parser. Extra options

--- parse_log_file.py
+++ parse_log_file.py
@@ -15,23 +15,69 @@ import platform
 import re
 
 from operator import itemgetter
- 
-def main():
 
-    comm_args = parse_args()
+def cmake_parse(content,sortfiles=None,sortopts=None,macros=None,fullpath=None):
 
-    logfile = comm_args['logfile']
-    verbose = comm_args['verbose']
-    do_sort = comm_args['sorted']
+    match_F90 = re.compile('^cd.*(gfortran|ifort|nagfor|pgfortran).*(\.f90|\.F90|\.F|\.f)\.o$')
+    F90files = filter(match_F90.match,content)
+    #print F90files
 
-    with open(logfile) as f:
-        content = [line.rstrip() for line in f]
+    rgx_list = [
+        # Peel off the cd and compiler
+        '^cd.*(gfortran|ifort|nagfor|pgfortran)\s',
 
-    #print content
+        # Remove esma_timer
+        '^.*esma_timer.sh\s+[\w]*\.o\srun', 
 
-    # This will have each line in our file as a separate member of a list
-    #loginp = [[i] for i in content]
-    #print loginp
+        # Match -I/ directories
+        '\s+-I/[\w/.-]*',
+
+        # Match -Iword directories
+        '-I[\w/.-]*\s+',
+
+        # Match -I.
+        r'-I.', 
+
+        # Match -Iword directories
+        '-J[\w/.-]*\s+',
+
+        # Remove -c
+        r'-c ', 
+
+        # Remove -o
+        r'-o\s+CMakeFiles.*$'
+        ]
+
+    if not macros:
+        rgx_list.append('-D[\w/.-=]*\s+')
+
+    output = []
+    for line in F90files:
+        new_line = line
+        for rgx in rgx_list:
+            new_line = re.sub(rgx,'',new_line)
+        new_line = re.sub(' +',' ',new_line)
+        new_line = new_line.rstrip()
+
+        splitline = new_line.rsplit(' ',1)
+
+        if not fullpath:
+            foundfile = splitline[-1]
+            splitline[-1] = os.path.basename(foundfile)
+
+        if sortopts:
+            splitline[0] = ' '.join( sorted(splitline[0].split()) )
+
+        new_line_reversed = tuple(reversed(tuple(splitline)))
+        output.append(new_line_reversed)
+
+    if sortfiles:
+        namesort = itemgetter(0)
+        output = sorted(output,key=namesort)
+
+    return output
+
+def gmake_parse(content,sortfiles=None,sortopts=None,macros=None):
 
     match_F90 = re.compile('.*mpi(fort|f90|f08|ifort).*(___\.f90|\.F90|\.F|\.f)$')
     F90files = filter(match_F90.match,content)
@@ -51,7 +97,7 @@ def main():
         r'-I.',
 
         # Match -D macros
-        '\s+-D[\w/.-=]*', 
+        #'\s+-D[\w/.-=]*', 
 
         # Remove mpi(fort|ifort|f90)
         'mpi(fort|ifort|f90|f08)\s+',
@@ -63,30 +109,67 @@ def main():
         r'-c '
         ]
 
+    if not macros:
+        rgx_list.append('-D[\w/.-=]*\s+')
+
     output = []
     for line in F90files:
         new_line = line
         for rgx in rgx_list:
             new_line = re.sub(rgx,'',new_line)
         new_line = re.sub(' +',' ',new_line)
-        new_line_reversed = tuple(reversed(tuple(new_line.rsplit(' ',1))))
+        new_line = new_line.rstrip()
+
+        splitline = new_line.rsplit(' ',1)
+
+        if sortopts:
+            splitline[0] = ' '.join( sorted(splitline[0].split()) )
+
+        new_line_reversed = tuple(reversed(tuple(splitline)))
         output.append(new_line_reversed)
 
-    if do_sort:
+    if sortfiles:
         namesort = itemgetter(0)
         output = sorted(output,key=namesort)
+
+
+    return output
+ 
+def main():
+
+    comm_args = parse_args()
+
+    logfile  = comm_args['logfile']
+    verbose  = comm_args['verbose']
+    sortfiles  = comm_args['sortfiles']
+    sortopts  = comm_args['sortopts']
+    macros   = comm_args['macros']
+    gmake    = comm_args['gmake']
+    cmake    = comm_args['cmake']
+    ninja    = comm_args['ninja']
+    fullpath = comm_args['fullpath']
+
+    with open(logfile) as f:
+        content = [line.rstrip() for line in f]
+
+    if cmake:
+        output = cmake_parse(content,sortfiles,sortopts,macros,fullpath)
+    elif ninja:
+        output = ninja_parse(content,sortfiles,sortopts,macros)
+    else:
+        output = gmake_parse(content,sortfiles,sortopts,macros)
+
+    #print content
+
+    # This will have each line in our file as a separate member of a list
+    #loginp = [[i] for i in content]
+    #print loginp
 
     for item in output:
         try:
             print ("File: {}\n   Options: {}\n").format(*item)
         except IndexError:
             print ("File: {}\n   Options: NONE\n").format(item[0])
-
-
-
-
-
-
 
 def parse_args():
 
@@ -105,30 +188,32 @@ def parse_args():
     # ------------------
     # Required Arguments
     # ------------------
-
     p.add_argument('logfile', type=str, help="Log File to Parse")
 
     # Sort Output
     # -----------
-    p.add_argument('--sorted', help="Print sorted output (by filename)", action='store_true')
+    p.add_argument('--sortfiles', help="Print sorted output (by filename)", action='store_true')
+    p.add_argument('--sortopts', help="Sort options", action='store_true')
 
-    ## ------------------
-    ## Optional Arguments
-    ## ------------------
+    # Sort Output
+    # -----------
+    p.add_argument('--macros', help="Include -D macros", action='store_true')
 
-    ## Experiment Description
-    ## ----------------------
-    #p.add_argument('--expdsc', help="Experiment Description (Default: same as expid)", type=str)
+    # GNU Make Log
+    # ------------
+    p.add_argument('--gmake', help="GNU Make Log",   action='store_true')
 
-    #try:
-        #with open(os.path.expanduser('~/.EXPDIRroot'),'r') as expdirroot:
-            #default_root = expdirroot.read().strip()
-    #except IOError:
-        #default_root = None
+    # cmake Make Log
+    # --------------
+    p.add_argument('--cmake', help="Cmake Make Log", action='store_true')
 
-    ## Experiment Directory
-    ## --------------------
-    #p.add_argument('--expdir', help="Experiment Directory Root *NOT CONTAINING EXPID* (Default is what is in ~/.EXPDIRroot: %s )" % default_root, default=default_root, type=str)
+    # ninja Make Log
+    # --------------
+    p.add_argument('--ninja', help="Ninja Make Log", action='store_true')
+
+    # cmake Make Log
+    # --------------
+    p.add_argument('--fullpath', help="Output full paths (cmake only)", action='store_true')
 
     ## Atmospheric Horizontal Resolution
     ## ---------------------------------
